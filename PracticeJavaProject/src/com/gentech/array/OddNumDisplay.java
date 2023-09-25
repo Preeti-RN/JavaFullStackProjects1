f46e0d472d3c0cb561a5e2347c7a72f2a3c7d00e
Preeti-RN <namagondpreeti@gmail.com>
2023-09-25 14:07:28 +0530

    EvenNumShow

---
+++ PracticeJavaProject/src/com/gentech/array/OddNumDisplay.java
@@ -0,0 +1,16 @@
+package com.gentech.array;
+
+public class OddNumDisplay {
+
+	public static void main(String[] args) {
+		for(int i=10;i<=30;i++)
+		{
+			if(i%2==1)
+			{
+				System.out.print(i+" ");
+			}
+		}
+
+	}
+
+}
