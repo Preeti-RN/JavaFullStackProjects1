d670c29504816a1da75e9a76313105fe227331b3
Preeti-RN <namagondpreeti@gmail.com>
2023-09-25 13:34:52 +0530

    singleton

---
+++ PracticeJavaProject/src/com/gentech/array/EvenNum.java
@@ -0,0 +1,15 @@
+package com.gentech.array;
+
+public class EvenNum {
+
+	public static void main(String[] args) {
+		for(int i=10;i<=30;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+}
